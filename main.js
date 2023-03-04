0342bc4da02096146214223da20c81dc8cb8a5ea
Yozadan <98656290+Yozadan@users.noreply.github.com>
2023-03-03 20:42:14 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+video = "";
+function preload(){
+video = createVideo('video.mp4');
+video.hide();
+}
+
+function setup(){
+canvas = createCanvas(330,330);
+canvas.center();
+
+}
+
+function draw(){
+image(video,0,0,330,330);
+}
+function start(){
+    objectDetector = ml5.objectDetector('cocossd',modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecing Objects";
+}
+function modelLoaded(){
+console.log("Model Loaded !");
+status = true;
+video.loop();
+video.speed(1);
+video.volume(0);
+}
